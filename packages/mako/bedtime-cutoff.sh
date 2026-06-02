8434246223531ba5a43d6daaedd687cdfb462aae
Joe Pringle <joe.pringle@proton.me>
2026-06-02 20:23:55 +0100

    fix attempt 1

--- packages/mako/bedtime-cutoff.sh
+++ packages/mako/bedtime-cutoff.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-add_cron() {
-    (crontab -l 2>/dev/null; echo "$1") | crontab -
+script_dir=$1
+
+add_cron_if_missing() {
+    if ! crontab -l 2>/dev/null | grep -qF "$1"; then
+        (crontab -l 2>/dev/null; echo "$1") | crontab -
+    fi
 }
 
-bedtime_netoff="30 21 * * * nmcli networking off"
-bedtime_shutdown='15 21 * * * /home/joep/.config/cron/bedtime-shutdown-job.sh'
+# bedtime_netoff="30 21 * * * nmcli networking off"
+bedtime_shutdown="15 21 * * * $script_dir/cron.sh"
 
-add_cron "$bedtime_shutdown"
+add_cron_if_missing "$bedtime_shutdown"
---
+++ packages/mako/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+script_dir="$HOME/.dotfiles/packages/mako"
+
+"$script_dir"/bedtime-cutoff.sh "$script_dir"
